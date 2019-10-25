ee6223e39c34f53f0a72b2f7087ee25395658c21
Leandro <leandro.frigerio@devugees.org>
2019-10-25 10:25:37 +0200

    Replace getElementById with querySelector

--- js/index.js
+++ js/index.js
@@ -1,15 +1,15 @@
-var greeting = document.getElementById('greeting');
+var greeting = document.querySelector('#greeting');
 greeting.innerHTML = 'Welcome to Weekend Countdown!';
 
-var goBtn = document.getElementById('go-btn');
+var goBtn = document.querySelector('#go-btn');
 
-goBtn.onclick = function() {
-  var userName = document.getElementById('username');
-  var promptQuestion = document.getElementById('prompt-question');
+goBtn.onclick = function () {
+  var userName = document.querySelector('#username');
+  var promptQuestion = document.querySelector('#prompt-question');
 
   promptQuestion.innerHTML = `Hello ${userName.value}. Today is ${
     getDayName()[0]
-  }. Only ${getDayName()[1]} days left until weekend!`;
+    }. Only ${getDayName()[1]} days left until weekend!`;
 
   function getDayName() {
     var days = [
