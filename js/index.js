e090182eb305e15139b4f1e6cc1d7c529b8a4fca
Leandro <leandro.frigerio@devugees.org>
2019-10-25 10:52:02 +0200

    eventListener instead of onclick

--- js/index.js
+++ js/index.js
@@ -1,19 +1,22 @@
 const greeting = document.querySelector('#greeting');
 greeting.innerHTML = 'Welcome to Weekend Countdown!';
-
 const goBtn = document.querySelector('#go-btn');
-
-const date = new Date();
 
 getDayName = () => {
   //https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date/toLocaleDateString
   return date.toLocaleDateString('en-EN', { weekday: 'long' });
 }
 
-goBtn.onclick = () => {
+getDaysToWeekend = () => {
+  // Date Global Object
+  // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date
+  const date = new Date();
   const userName = document.querySelector('#username');
   const promptQuestion = document.querySelector('#prompt-question');
 
   promptQuestion.innerHTML = `Hello ${userName.value}. Today is ${getDayName()}. Only ${5 - date.getDay()} days left until weekend!`;
 
 };
+
+// https://developer.mozilla.org/en-US/docs/Web/API/EventTarget/addEventListener
+goBtn.addEventListener("click", getDaysToWeekend);
