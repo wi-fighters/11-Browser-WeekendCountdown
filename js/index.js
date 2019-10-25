b1f3eb62bed82f01faff0d3a844d02fe1332b73b
Leandro <leandro.frigerio@devugees.org>
2019-10-25 10:42:04 +0200

    Refactoring and documentation

--- js/index.js
+++ js/index.js
@@ -1,29 +1,19 @@
-var greeting = document.querySelector('#greeting');
+const greeting = document.querySelector('#greeting');
 greeting.innerHTML = 'Welcome to Weekend Countdown!';
 
-var goBtn = document.querySelector('#go-btn');
+const goBtn = document.querySelector('#go-btn');
 
-goBtn.onclick = function () {
-  var userName = document.querySelector('#username');
-  var promptQuestion = document.querySelector('#prompt-question');
+const date = new Date();
 
-  promptQuestion.innerHTML = `Hello ${userName.value}. Today is ${
-    getDayName()[0]
-    }. Only ${getDayName()[1]} days left until weekend!`;
+getDayName = () => {
+  //https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date/toLocaleDateString
+  return date.toLocaleDateString('en-EN', { weekday: 'long' });
+}
 
-  function getDayName() {
-    var days = [
-      'Monday',
-      'Tuesday',
-      'Wednesday',
-      'Thursday',
-      'Friday',
-      'Saturday',
-      'Sunday'
-    ];
+goBtn.onclick = () => {
+  const userName = document.querySelector('#username');
+  const promptQuestion = document.querySelector('#prompt-question');
 
-    var date = new Date();
-    var day = date.getDay();
-    return [days[day - 1], 5 - day];
-  }
+  promptQuestion.innerHTML = `Hello ${userName.value}. Today is ${getDayName()}. Only ${5 - date.getDay()} days left until weekend!`;
+
 };
